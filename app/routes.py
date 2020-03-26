9e25aa8f2bc94399f087e91d5856714027a41dc3
charliepianist <charliepianist@yahoo.com>
2020-03-25 23:01:18 -0400

    Finished header

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,23 @@ from flask import send_from_directory
 from flask import make_response, render_template, request
 from flask import url_for, redirect
 
+# Temporary - just redirects to login page as of now
 @app.route('/')
+def home():
+    return redirect(url_for('login_form'))
+
+# Login page. Should redirect automatically to homepage if logged in already, but currently does not
 @app.route('/login')
-# Home page with login screen. Should be different if logged in, this is just the login screen as of now
-def homepage():
-    html = render_template('homepage.html',
-        title='Home | Spot')
+def login_form():
+    html = render_template('login.html',
+        title='Login | Spot')
+    response = make_response(html)
+    return response
+
+# Registration page. Temporarily is the same as Login page.
+@app.route('/register')
+def register_form():
+    html = render_template('login.html',
+        title='Register | Spot')
     response = make_response(html)
     return response
