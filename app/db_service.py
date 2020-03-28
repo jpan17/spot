2bb1407ec489b486c3b1424aff0f8778897260ff
charliepianist <charliepianist@yahoo.com>
2020-03-28 17:40:55 -0400

    Fleshed out get_listings

--- app/db_service.py
+++ app/db_service.py
@@ -67,13 +67,42 @@ def get_listings(user = None, user_id = -1, accepted = False):
 
     Parameters
     ----------
-    user : User, optional
+    user : User or None, optional
         The user to return the listings of. Note that one of *user* and *user_id* must
         be specified in a call to this function.
-    user_id : int, optional
-        The ID of the user to return the listings of, if *user* is not specified. Note that
-        one of *user* and *user_id* must be specified in a call to this function.
-    accepted : boolean, optional
+    user_id : int or str, optional
+        The ID of the user to return the listings of, if *user* is not specified or None. 
+        Note that one of *user* and *user_id* must be specified in a call to this function.
+    accepted : bool, optional
+        If the user is both an owner and a sitter, the accepted listings will be returned if
+        *accepted* is True. Defaults to False, in which case the listings that the user owns
+        are returned. Ignored if the user is not both an owner and a sitter.
+
+    Returns
+    -------
+    list of Listing
+        A (potentially empty) list of all of the user's Listings. Whether the list is of
+        owned Listings or accepted Listings depends on the parameters, described above.
+
+    Raises
+    ------
+    TypeError
+        If *user* is not of type User or None, if *user* is None/unspecified and *user_id* 
+        is not of type int or str, or if *accepted* is not of type bool.
+    ValueError
+        If *user* is None/unspecified and *user_id* is a str that cannot be parsed to an int.
+
+    Examples
+    --------
+    >>> db_service.get_listings(User(id=1))
+    [<Listing ID=1>, <Listing ID=2>]
+
+    >>> db_service.get_listings(user_id='2')
+    [<Listing ID=3>]
+
+    >>> db_service.get_listings()
+    TypeError('No user was provided and no user was found by the given user_id (or no user_id was provided)')
+
     """
 
     # If user unspecified, search for it
