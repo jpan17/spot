8f5cf457fd2f56b275b14859cab83ae9c746d3a4
charliepianist <charliepianist@yahoo.com>
2020-03-29 13:50:52 -0400

    Replaced grooming and bathing with cleaning

--- enums.py
+++ enums.py
@@ -4,4 +4,4 @@
 import enum
 
 pet_types = ('Dog', 'Cat', 'Rodent', 'Fish', 'Bird', 'Reptile', 'Amphibian', 'Other')
-activities = ('Feeding', 'Walking', 'Playing', 'Grooming', 'Bathing', 'Other')
+activities = ('Feeding', 'Walking', 'Playing', 'Cleaning', 'Other')
