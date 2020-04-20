de98cae13e3fcbfb17555103cd8a3014891f7b6c
charliepianist <charliepianist@yahoo.com>
2020-04-20 16:44:24 -0400

    Clear filters button and cleaned up code

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,49 +1,39 @@
-function clear_pet_types(...pet_types) {
-    for(var i = 0; i < pet_types.length; i++) {
-        $('#pet_type_' + pet_types[i]).prop("checked", false);
-    }
+// Uncheck all pet_types
+function clear_pet_types() {
+    $('#pet_type_filter_button [id^="pet_type_"]:checked').prop("checked", false);
     $('#pet_type_filter_button').removeClass('has-filters');
 }
 
-function pet_types_dropdown_click(event, ...pet_types) {
+// Register click on pet_types_dropdown (including each of the checkboxes)
+function pet_types_dropdown_click(event) {
     event.stopPropagation();
-    // Are any of the pet_types checked?
-    var anyChecked = false;
-    for(var i = 0; i < pet_types.length; i++) {
-        if($('#pet_type_' + pet_types[i]).prop("checked")) {
-            anyChecked = true;
-            break;
-        }
-    }
-    // Change the display of the filter as appropriate
-    if(anyChecked) {
-        $('#pet_type_filter_button').addClass('has-filters');
-    }else {
+    // Add class if and only if any checked pet_types
+    if($('#pet_type_filter_button [id^="pet_type_"]:checked').length === 0) {
         $('#pet_type_filter_button').removeClass('has-filters');
+    }else {
+        $('#pet_type_filter_button').addClass('has-filters');
     }
 }
 
-function clear_activities(...activities) {
-    for(var i = 0; i < activities.length; i++) {
-        $('#activity_' + activities[i]).prop("checked", false);
-    }
+// Uncheck all activities
+function clear_activities() {
+    $('#activities_filter_button [id^="activity_"]:checked').prop("checked", false);
     $('#activities_filter_button').removeClass('has-filters');
 }
 
-function activities_dropdown_click(event, ...activities) {
+// Register click on activities dropdown (including each of the checkboxes)
+function activities_dropdown_click(event) {
     event.stopPropagation();
     // Are any of the activities checked?
-    var anyChecked = false;
-    for(var i = 0; i < activities.length; i++) {
-        if($('#activity_' + activities[i]).prop("checked")) {
-            anyChecked = true;
-            break;
-        }
-    }
-    // Change the display of the filter as appropriate
-    if(anyChecked) {
-        $('#activities_filter_button').addClass('has-filters');
-    }else {
+    if($('#activities_filter_button [id^="activity_"]:checked').length === 0) {
         $('#activities_filter_button').removeClass('has-filters');
+    }else {
+        $('#activities_filter_button').addClass('has-filters');
     }
+}
+
+// Clear all filters
+function clear_all_filters() {
+    clear_pet_types();
+    clear_activities();
 }
