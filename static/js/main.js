fa276638b5b0427fb49ac333d7e274cbb411a37c
charliepianist <charliepianist@yahoo.com>
2020-04-22 18:34:16 -0400

    Customized file upload input (looks much better now)

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,16 @@
 var Spot = {
+    setup: function() {
+        // Implement custom file inputs to display uploaded file
+        $('.file-input input[type=file]').change(function() {
+            var file = $(this).prop('files')[0].name;
+            if(file.length > 20) file = file.substring(0, 17) + '...';
+            $(this).siblings('.file-name').text(file);
+        })
+    },
+
     redirect: function(url) {
         window.location.href = url;
     }
 }
+
+$(document).ready(Spot.setup);
